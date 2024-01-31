e646d4b233e50e3a83a5ff19ced7b8cb74349520
BiagioCaputo <scebiagio98@gmail.com>
2024-01-31 15:34:14 +0100

    created title component and added to the template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script>
+import AppTitle from './components/AppTitle.vue'
 export default {
+  name: 'My-vue-app',
+
+  components: { AppTitle, }
 
 };
 </script>
 
 
-<template></template>
+<template>
+  <AppTitle />
+</template>
 <style></style>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -5,5 +5,10 @@ export default {
 </script>
 
 
-<template></template>
-<style scoped></style>
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+<style scoped> h1 {
+     text-align: center;
+ }
+</style>
